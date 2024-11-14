dc80689873525bcd50ba678504bf7622d49d1289
Francois Panganiban <francois.panganiban@estiam.com>
2024-11-14 13:30:08 +0100

    improve animation transition

--- src/app/features/home/home.routes.ts
+++ src/app/features/home/home.routes.ts
@@ -5,6 +5,7 @@ export const routes: Routes = [
   {
     path: '',
     component: HomeLayoutComponent,
+    data: { animation: 'HomePage' },
   },
 ];
 
--- src/app/shared/animations/route-transitions.ts
+++ src/app/shared/animations/route-transitions.ts
@@ -1,18 +1,26 @@
 import { animate, group, query, style, transition, trigger } from '@angular/animations';
 
-export const routeTransition = trigger('routeTransition', [
-  transition('* => *', [
-    query('.layout__loading-screen', [style({ transform: 'translateY(100%)' })]),
-    query('.footer', [style({ opacity: 0 })]),
+const animations = [
+  query('.layout__loading-screen', [style({ transform: 'translateY(100%)' })]),
+  query('.footer', [style({ opacity: 0 })]),
 
-    style({ position: 'relative' }),
-    query(':enter, :leave', [style({ position: 'absolute', top: '80px', left: 0, width: '100%' })], { optional: true }),
-    query(':enter', [style({ opacity: 0, transform: 'translateY(-20px)' })], { optional: true }),
+  style({ position: 'relative' }),
+  query(':enter, :leave', [style({ position: 'absolute', top: '80px', left: 0, width: '100%' })], { optional: true }),
+  query(':enter', [style({ opacity: 0, transform: 'translateY(-20px)' })], { optional: true }),
 
-    group([
-      query(':leave', [animate('500ms', style({ opacity: 0 }))], { optional: true }),
-      query('.layout__loading-screen', [animate('2000ms ease-out', style({ transform: 'translateY(-100%)' }))]),
-    ]),
-    query(':enter', [animate('500ms ease-out', style({ opacity: 1, transform: 'translateY(0px)' }))], { optional: true }),
+  group([
+    query(':leave', [animate('500ms', style({ opacity: 0 }))], { optional: true }),
+    query('.layout__loading-screen', [animate('2000ms ease-out', style({ transform: 'translateY(-100%)' }))]),
   ]),
+  query(':enter', [animate('500ms ease-out', style({ opacity: 1, transform: 'translateY(0px)' }))], { optional: true }),
+];
+
+export const routeTransition = trigger('routeTransition', [
+  transition('HomePage => *', animations),
+  transition('CharacterListPage => *', animations),
+  transition('CharacterDetailsPage => *', animations),
+  transition('EpisodeListPage => *', animations),
+  transition('EpisodeDetailsPage => *', animations),
+  transition('LocationListPage => *', animations),
+  transition('LocationDetailsPage => *', animations),
 ]);
